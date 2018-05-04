50871d216da155968d1bdfed3581916b28ddf0c9
Jonathan Crabtree <jonathancrabtree@gmail.com>
2018-05-04 12:22:48 -0400

    Fill in Dataset objects for RNA-Seq data subsets. Fix 'creators' fields.

--- bin/gtex_v7_datasets_to_dats.py
+++ bin/gtex_v7_datasets_to_dats.py
@@ -18,30 +18,84 @@ import sys
 # landing page for public RNA-Seq datasets
 GTEX_DATASETS_URL = "https://www.gtexportal.org/home/datasets"
 
-# "count" from Statistical Methods Ontology
+## Statistical Methods Ontology
+# "count"
 COUNT_TYPE = OrderedDict([("value", "count"), ("valueIRI", "http://purl.obolibrary.org/obo/STATO_0000047")])
 
+# "sequence read count"
+# TODO - strictly speaking the definition of this term says that it applies to a DNA sequencing assay, not an RNA sequencing assay
+# TODO - also not clear if different names can be associated with the same valueIRI (different from each other and different from the name of the indicated term)
+GENE_READ_COUNT_NAME = OrderedDict([("value", "Gene read counts"), ("valueIRI", "http://purl.obolibrary.org/obo/STATO_0000064")])
+TRANSCRIPT_READ_COUNT_NAME = OrderedDict([("value", "Transcript read counts"), ("valueIRI", "http://purl.obolibrary.org/obo/STATO_0000064")])
+EXON_READ_COUNT_NAME = OrderedDict([("value", "Exon read counts"), ("valueIRI", "http://purl.obolibrary.org/obo/STATO_0000064")])
+JUNCTION_READ_COUNT_NAME = OrderedDict([("value", "Junction read counts"), ("valueIRI", "http://purl.obolibrary.org/obo/STATO_0000064")])
+
+## Ontology for Biomedical Investigations
+# "transcription profiling assay"
+TRANSCRIPT_PROFILING_TYPE = OrderedDict([("value", "transcription profiling"), ("valueIRI", "http://purl.obolibrary.org/obo/OBI_0000424")])
+# "RNA-seq assay"
+RNASEQ_ASSAY_TYPE = OrderedDict([("valueIRI", "http://purl.obolibrary.org/obo/OBI_0001271")])
+# "Illumina"
+ILLUMINA_TYPE = OrderedDict([("value", "Illumina"), ("valueIRI", "http://purl.obolibrary.org/obo/OBI_0000759")])
+
+# documentation for earlier version at https://data.broadinstitute.org/cancer/cga/tools/rnaseqc/RNA-SeQC_Help_v1.1.2.pdf
 RNA_SEQ_QC = OrderedDict([
         ("@type", "Software"),
         ("name", "RNASeQC"),
         ("version", "v1.1.8")])
 
 # gene read counts Dimension
-READ_COUNTS_DIM = OrderedDict([
+GENE_READ_COUNTS_DIM = OrderedDict([
         ("@type", "Dimension"),
-        # "sequence read count" from Statistical Methods Ontology
-        # TODO - strictly speaking the definition of this term says that it applies to a DNA sequencing assay, not an RNA sequencing assay
-        ("name", OrderedDict([("value", "gene read counts"), ("valueIRI", "http://purl.obolibrary.org/obo/STATO_0000064")])),
+        ("name", GENE_READ_COUNT_NAME),
         ("description", "gene read counts"),
+        ("types", [COUNT_TYPE])
+        ])
+
+TRANSCRIPT_READ_COUNTS_DIM = OrderedDict([
+        ("@type", "Dimension"),
+        ("name", TRANSCRIPT_READ_COUNT_NAME),
+        ("description", "transcript read counts"),
+        ("types", [COUNT_TYPE])
+        ])
+
+EXON_READ_COUNTS_DIM = OrderedDict([
+        ("@type", "Dimension"),
+        ("name", EXON_READ_COUNT_NAME),
+        ("description", "exon read counts"),
         ("types", [COUNT_TYPE])
         ])
 
 # junction count Dimension
 JUNCTION_COUNT_DIM = OrderedDict([
         ("@type", "Dimension"),
-        # "sequence read count" from Statistical Methods Ontology
-        ("name", OrderedDict([("value", "Junction read counts"), ("valueIRI", "http://purl.obolibrary.org/obo/STATO_0000064")])),
+        # TODO - is there a more appropriate ontology term for this?
+        ("name", JUNCTION_READ_COUNT_NAME),
         ("description", "Junction read counts"),
+        ("types", [COUNT_TYPE])
+        ])
+
+# gene TPM Dimension
+# TODO - is this transcripts per _gene_ kilobase million?
+GENE_TPM_DIM = OrderedDict([
+        ("@type", "Dimension"),
+        ("name", OrderedDict([("value", "Transcripts Per Kilobase Million"), ("valueIRI", "")])),
+        ("description", "Transcripts Per Kilobase Million"),
+        ("types", [COUNT_TYPE])
+        ])
+
+TISSUE_MEDIAN_TPM_DIM = OrderedDict([
+        ("@type", "Dimension"),
+        ("name", OrderedDict([("value", "Tissue Median Transcripts Per Kilobase Million"), ("valueIRI", "")])),
+        ("description", "Tissue Median Transcripts Per Kilobase Million"),
+        ("types", [COUNT_TYPE])
+        ])
+
+# transcript TPM Dimension
+TRANSCRIPT_TPM_DIM = OrderedDict([
+        ("@type", "Dimension"),
+        ("name", OrderedDict([("value", "Transcripts Per Kilobase Million"), ("valueIRI", "")])),
+        ("description", "Transcripts Per Kilobase Million"),
         ("types", [COUNT_TYPE])
         ])
 
@@ -50,15 +104,6 @@ STAR = OrderedDict([
         ("@type", "Software"),
         ("name", "STAR Aligner"),
         ("version", "v2.4.2a")
-        ])
-
-# transcript TPM Dimension
-TRANSCRIPT_TPM_DIM = OrderedDict([
-        ("@type", "Dimension"),
-        # "sequence read count" from Statistical Methods Ontology
-        ("name", OrderedDict([("value", "Transcripts Per Kilobase Million"), ("valueIRI", "")])),
-        ("description", "Transcripts Per Kilobase Million"),
-        ("types", [COUNT_TYPE])
         ])
 
 # RSEM Software
@@ -70,14 +115,21 @@ RSEM = OrderedDict([
 
 # Public RNA-Seq datasets listed at https://www.gtexportal.org/home/datasets
 RNASEQ_DATASETS = [
-    { "descr": "Gene read counts.", "file": "GTEx_Analysis_2016-01-15_v7_RNASeQCv1.1.8_gene_reads.gct.gz", "measures": [READ_COUNTS_DIM], "uses": [] },
-    { "descr": "Gene TPMs.", "file": "GTEx_Analysis_2016-01-15_v7_RNASeQCv1.1.8_gene_tpm.gct.gz", "measures": [], "uses": [] },
+    { "descr": "Gene read counts.", "file": "GTEx_Analysis_2016-01-15_v7_RNASeQCv1.1.8_gene_reads.gct.gz", 
+      "measures": [GENE_READ_COUNTS_DIM], "uses": [RNA_SEQ_QC] },
+    { "descr": "Gene TPMs.", "file": "GTEx_Analysis_2016-01-15_v7_RNASeQCv1.1.8_gene_tpm.gct.gz", 
+      "measures": [GENE_TPM_DIM], "uses": [RNA_SEQ_QC] },
     # TODO - this file was derived directly from the preceding one
-    { "descr": "Tissue median TPMs.", "file": "GTEx_Analysis_2016-01-15_v7_RNASeQCv1.1.8_gene_median_tpm.gct.gz", "measures": [], "uses": [] },
-    { "descr": "Junction read counts.", "file": "GTEx_Analysis_2016-01-15_v7_STARv2.4.2a_junctions.gct.gz", "measures": [JUNCTION_COUNT_DIM], "uses": [STAR] },
-    { "descr": "Transcript read counts.", "file": "GTEx_Analysis_2016-01-15_v7_RSEMv1.2.22_transcript_expected_count.txt.gz", "measures": [], "uses": [] },
-    { "descr": "Transcript TPMs.", "file": "GTEx_Analysis_2016-01-15_v7_RSEMv1.2.22_transcript_tpm.txt.gz", "measures": [TRANSCRIPT_TPM_DIM], "uses": [RSEM] },
-    { "descr": "Exon read counts.", "file": "GTEx_Analysis_2016-01-15_v7_RNASeQCv1.1.8_exon_reads.txt.gz", "measures": [], "uses": [] }
+    { "descr": "Tissue median TPMs.", "file": "GTEx_Analysis_2016-01-15_v7_RNASeQCv1.1.8_gene_median_tpm.gct.gz", 
+      "measures": [TISSUE_MEDIAN_TPM_DIM], "uses": [RNA_SEQ_QC] },
+    { "descr": "Junction read counts.", "file": "GTEx_Analysis_2016-01-15_v7_STARv2.4.2a_junctions.gct.gz", 
+      "measures": [JUNCTION_COUNT_DIM], "uses": [STAR] },
+    { "descr": "Transcript read counts.", "file": "GTEx_Analysis_2016-01-15_v7_RSEMv1.2.22_transcript_expected_count.txt.gz", 
+      "measures": [TRANSCRIPT_READ_COUNTS_DIM], "uses": [RSEM] },
+    { "descr": "Transcript TPMs.", "file": "GTEx_Analysis_2016-01-15_v7_RSEMv1.2.22_transcript_tpm.txt.gz", 
+      "measures": [TRANSCRIPT_TPM_DIM], "uses": [RSEM] },
+    { "descr": "Exon read counts.", "file": "GTEx_Analysis_2016-01-15_v7_RNASeQCv1.1.8_exon_reads.txt.gz", 
+      "measures": [EXON_READ_COUNTS_DIM], "uses": [RNA_SEQ_QC] }
 ]
 
 DB_GAP = OrderedDict([("@type", "DataRepository"), ("name", "dbGaP")])
@@ -95,12 +147,9 @@ GTEX_V7_RELEASE_DATE = OrderedDict([
         ])
 
 GTEX_V7_TYPES = [OrderedDict([
-            # Ontology for Biomedical Investigations / transcription profiling assay
-            ("information", OrderedDict([("value", "transcription profiling"), ("valueIRI", "http://purl.obolibrary.org/obo/OBI_0000424")])),
-            # Ontology for Biomedical Investigations / RNA-seq assay
-            ("method", OrderedDict([("value", "RNA-seq assay"), ("valueIRI", "http://purl.obolibrary.org/obo/OBI_0001271")])),
-            # Ontology for Biomedical Investigations / Illumina
-            ("platform", OrderedDict([("value", "Illumina"), ("valueIRI", "http://purl.obolibrary.org/obo/OBI_0000759")]))
+            ("information", TRANSCRIPT_PROFILING_TYPE),
+            ("method", RNASEQ_ASSAY_TYPE),
+            ("platform", ILLUMINA_TYPE)
             ])]
 
 # ------------------------------------------------------
@@ -129,7 +178,7 @@ def write_datasets_json(output_file):
                 ("title", "GTEx v7 RNA-Seq Analysis, " + descr),
                 ("storedIn", DB_GAP),
                 ("types", GTEX_V7_TYPES),
-                ("creators", GTEX_CONSORTIUM),
+                ("creators", [GTEX_CONSORTIUM]),
                 # TODO - where does the actual filename belong?
                 ("distributions", [OrderedDict([
                                 ("@type", "DatasetDistribution"),
@@ -156,7 +205,7 @@ def write_datasets_json(output_file):
             ("title",  "GTEx v7 RNA-Seq Analysis"),
             ("storedIn", DB_GAP),
             ("types", GTEX_V7_TYPES),
-            ("creators", GTEX_CONSORTIUM),
+            ("creators", [GTEX_CONSORTIUM]),
             ("distributions", [OrderedDict([
                                     ("@type", "DatasetDistribution"),
                                     ("access", OrderedDict([
