389b48729e4f6b50ac4febb6532caf955eb8a932
Jonathan Crabtree <jonathancrabtree@gmail.com>
2018-08-15 12:01:36 -0400

    Move subjects and samples one level down, use JSON-LD id refs to reduce redundancy.

--- bin/gtex_v7_to_dats.py
+++ bin/gtex_v7_to_dats.py
@@ -46,9 +46,19 @@ def main():
     # link subjects to samples
     ccmm.gtex.rna_extracts.link_samples_to_subjects(samples, subjects)
 
-    # add samples to top-level Dataset
+    # samples
     samples_json = ccmm.gtex.rna_extracts.get_samples_json(samples, subjects)
-    rnaseq_dataset.set('isAbout', samples_json)
+    # JSON-LD id references for samples
+    sample_refs_json = [ s.getIdRef() for s in samples_json ]
+
+    # add samples and sample refs to 2nd-level Datasets
+    data_subsets = rnaseq_dataset.get("hasPart")
+    # add full samples JSON to the first 2nd-level Dataset
+    data_subsets[0].set('isAbout', samples_json)
+
+    # use JSON-LD id references for the rest:
+    for ds in data_subsets[1:]:
+        ds.set('isAbout', sample_refs_json)
 
     # write Dataset to DATS JSON file
     with open(args.output_file, mode="w") as jf:
