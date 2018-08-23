81c475c0bd012554e15870cb50453e867cba36d9
Jonathan Crabtree <jonathancrabtree@gmail.com>
2018-08-23 19:51:36 -0400

    Update SPARQL wrapper scripts for v0.4

--- sparql/run-sparql-queries.sh
+++ sparql/run-sparql-queries.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-# takes about 60 seconds using GitHub context URIs
-time ./topmed_sparql_examples.py --topmed_file=./TOPMed_phs000946_wgs_public_v0.4preview3.jsonld
+tar xzvf ../releases/KC7-crosscut-metadata-v0.4.tgz
+export JSON_DIR=KC7-crosscut-metadata-v0.4/data/datasets/
+
+# takes about 1m35s using GitHub context URIs
+time ./topmed_gtex_sparql_examples.py --dats_file=$JSON_DIR/GTEx_v7_rnaseq_20_SAMPLES_public.jsonld
+
 
 
