529979700cfcc759fcde3f4c23c47822118ce59d
Jonathan Crabtree <jonathancrabtree@gmail.com>
2018-08-23 17:14:18 -0400

    Update master BDBag creation script.

--- make-crosscut-instance-bdbag.sh
+++ make-crosscut-instance-bdbag.sh
@@ -8,66 +8,76 @@ setenv PYTHONPATH ./
 # BDBag that contains DATS JSON-LD files that describe the metadata from the 
 # following resources:
 #
-# 1. Public mouse C57BL/6J reference genome and human homologs from MGI/MGD.
-# 2. Public GTEx v7 RNA-Seq metadata.
-# 3. Public TOPMed metadata from non-access-restricted dbGaP files.
+# 1. Public GTEx v7 metadata from dbGaP and the GTEx portal.
+# 2. Public TOPMed metadata from non-access-restricted dbGaP files.
 # 
-# In the case of TOPMed the crosscut metadata model instance may be expanded
-# to include access-restricted dbGaP metadata (see the relevant script 
+# In the case of both GTEx and TOPMed the crosscut metadata model instance may 
+# be expanded to include access-restricted dbGaP metadata (see the relevant script 
 # invocation below) but this expanded instance may not be publicly distributed.
 
 setenv VERSION 0.4
 setenv EXTERNAL_ID "KC7-crosscut-metadata-v${VERSION}"
-setenv EXTERNAL_DESCR "v${VERSION} release of the KC7 crosscut metadata model for MGI, GTEx v7, and TOPMed public metadata using DATS v2.2+"
+setenv EXTERNAL_DESCR "v${VERSION} release of the KC7 crosscut metadata model for GTEx v7 and TOPMed public metadata using DATS v2.2+"
 
 # set up internal bag structure
 mkdir -p $EXTERNAL_ID/docs
 mkdir -p $EXTERNAL_ID/datasets
 
 ## -----------------------------------------------
-## Public mouse C57BL/6J reference genome
-## -----------------------------------------------
-
-# retrieve the requisite flat files from MGI/MGD:
-#mkdir -p mgd-data
-#cd mgd-data
-#curl -O http://www.informatics.jax.org/downloads/mgigff/MGI.gff3.gz
-#curl -O http://www.informatics.jax.org/downloads/reports/HOM_MouseHumanSequence.rpt
-#cd ..
-
-# convert the MGI metadata to DATS JSON-LD
-./bin/mgd_to_dats.py --gff3_path=mgd-data/MGI.gff3.gz --human_homologs_path=mgd-data/HOM_MouseHumanSequence.rpt --output_file=$EXTERNAL_ID/datasets/MGD_GRCm38-C57BL6J_public.json
-
-## -----------------------------------------------
 ## Public GTEx v7 RNA-Seq metadata
 ## -----------------------------------------------
 
-# this script expects the following two GTEx metadata files 
+# This script requires the following two GTEx metadata files 
 # (from https://www.gtexportal.org/home/datasets to be in the current working directory:
 #
 # GTEx_v7_Annotations_SubjectPhenotypesDS.txt
 # GTEx_v7_Annotations_SampleAttributesDS.txt
 
-./bin/gtex_v7_to_dats.py --output_file=$EXTERNAL_ID/datasets/GTEx_v7_rnaseq_public.json
+./bin/gtex_v7_to_dats.py --output_file=$EXTERNAL_ID/datasets/GTEx_v7_rnaseq_public.jsonld 
+
+# limit to 20 samples to create smaller file for test purposes
+#./bin/gtex_v7_to_dats.py --output_file=$EXTERNAL_ID/datasets/GTEx_v7_rnaseq_20_SAMPLES_public.jsonld --max_output_samples=20
+
+## -----------------------------------------------
+## Public GTEx v7 dbGaP metadata
+## -----------------------------------------------
+
+# Convert public dbGaP metadata for GTEx to DATS JSON.
+#
+# First retrieve the pheno_variable_summaries files for GTEx into a local directory:
+#  1. create local directory dbgap-data if it does not already exist
+#  2. pull ftp://ftp.ncbi.nlm.nih.gov/dbgap/studies/phs000424/phs000424.v7.p2/pheno_variable_summaries/ into dbgap-data/phs000424.v7.p2
+#  3. run the command below
+
+./bin/dbgap_gtex_to_dats.py --dbgap_public_xml_path=./dbgap-data/phs000424.v7.p2 --output_file=$EXTERNAL_ID/datasets/GTEx_v7_dbGaP_public.jsonld
 
 ## -----------------------------------------------
 ## Public TOPMed metadata
 ## -----------------------------------------------
 
-# convert public TOPMed metadata for phs000946 to DATS JSON
-# retrieve v3 variable summaries from ftp://ftp.ncbi.nlm.nih.gov/dbgap/studies/phs000946/phs000946.v3.p1/pheno_variable_summaries/ into ./phs000946.v3, then:
+# Convert public dbGaP metadata for selected TOPMed studies to DATS JSON.
+#
+# First retrieve the pheno_variable_summaries from the desired TOPMed studies into a local directory: 
+#  1. create local directory dbgap-data if it does not already exist
+#  2. pull ftp://ftp.ncbi.nlm.nih.gov/dbgap/studies/phs001024/phs001024.v3.p1/pheno_variable_summaries/ into dbgap-data/phs001024.v3.p1
+#  3. pull ftp://ftp.ncbi.nlm.nih.gov/dbgap/studies/phs000951/phs000951.v2.p2/pheno_variable_summaries/ into dbgap-data/phs000951.v2.p2
+#  4. pull ftp://ftp.ncbi.nlm.nih.gov/dbgap/studies/phs000179/phs000179.v5.p2/pheno_variable_summaries/ into dbgap-data/phs000179.v5.p2
+#  5. run the command below
 
-./bin/topmed_to_dats.py --dbgap_public_xml_path=./phs000946.v3 --output_file=$EXTERNAL_ID/datasets/TOPMed_phs000946_wgs_public.json
+./bin/topmed_to_dats.py --dbgap_accession_list='phs001024.v3.p1,phs000951.v2.p2,phs000179.v5.p2' \
+  --dbgap_public_xml_path=./dbgap-data \
+  --output_file=$EXTERNAL_ID/datasets/TOPMed_phs000951_phs000946_phs001024_wgs_public.jsonld
 
 ## -----------------------------------------------
 ## RESTRICTED ACCESS TOPMed metadata
 ## -----------------------------------------------
 
-# convert RESTRICTED ACCESS TOPMed metadata for phs000946 to DATS JSON
-# retrieve v3 variable summaries and access restricted metadata into ./phs000946.v3, then run:
+# Convert RESTRICTED ACCESS TOPMed metadata to DATS JSON.
 
-# ./bin/topmed_to_dats.py --dbgap_public_xml_path=./phs000946.v3 --dbgap_protected_metadata_path=./phs000946.v3 \
-#  --output_file=$EXTERNAL_ID/metadata/annotations/datasets/TOPMed_phs000946_wgs_RESTRICTED.json
+#./bin/topmed_to_dats.py --dbgap_accession_list='phs001024.v3.p1,phs000951.v2.p2,phs000179.v5.p2' \
+#  --dbgap_public_xml_path=./dbgap-data \
+#  --dbgap_protected_metadata_path=./restricted-access-dbgap-data \
+#  --output_file=$EXTERNAL_ID/datasets/TOPMed_phs000951_phs000946_phs001024_wgs_RESTRICTED.jsonld
 
 ## -----------------------------------------------
 ## Add documentation
