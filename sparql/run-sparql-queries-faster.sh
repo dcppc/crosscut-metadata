3583e9d8055ced7dfd61ee4739c34f0e943c13b1
Jonathan Crabtree <jonathancrabtree@gmail.com>
2018-08-24 16:50:08 -0400

    Add full size GTEX_v7_rnaseq JSON-LD file.

--- sparql/run-sparql-queries-faster.sh
+++ sparql/run-sparql-queries-faster.sh
@@ -8,8 +8,9 @@ export JSON_DIR=KC7-crosscut-metadata-v0.4/data/datasets/
 # download context files locally
 git clone git@github.com:datatagsuite/context.git
 
-# rewrite JSON-LD file to use local contexts
+# rewrite JSON-LD files to use local contexts
 perl -ne 's/https:\/\/(datatagsuite.github.io|w3id.org)\//.\//; print;' <$JSON_DIR/GTEx_v7_rnaseq_20_SAMPLES_public.jsonld >$JSON_DIR/GTEx_v7_rnaseq_20_SAMPLES_public_local_contexts.jsonld
+perl -ne 's/https:\/\/(datatagsuite.github.io|w3id.org)\//.\//; print;' <$JSON_DIR/GTEx_v7_rnaseq_public.jsonld >$JSON_DIR/GTEx_v7_rnaseq_public_local_contexts.jsonld
 perl -ne 's/https:\/\/(datatagsuite.github.io|w3id.org)\//.\//; print;' <$JSON_DIR/TOPMed_phs000951_phs000946_phs001024_wgs_public.jsonld >$JSON_DIR/TOPMed_phs000951_phs000946_phs001024_wgs_public_local_contexts.jsonld
 perl -ne 's/https:\/\/(datatagsuite.github.io|w3id.org)\//.\//; print;' <$JSON_DIR/GTEx_v7_dbGaP_public.jsonld >$JSON_DIR/GTEx_v7_dbGaP_public_local_contexts.jsonld
 
@@ -19,3 +20,5 @@ time ./topmed_gtex_sparql_examples.py --dats_file=$JSON_DIR/GTEx_v7_rnaseq_20_SA
 time ./topmed_gtex_sparql_examples.py --dats_file=$JSON_DIR/TOPMed_phs000951_phs000946_phs001024_wgs_public_local_contexts.jsonld
 # takes about 6-7 seconds using local context files
 time ./topmed_gtex_sparql_examples.py --dats_file=$JSON_DIR/GTEx_v7_dbGaP_public_local_contexts.jsonld
+# takes about 6-8 minutes using local context files
+#time ./topmed_gtex_sparql_examples.py --dats_file=$JSON_DIR/GTEx_v7_rnaseq_public_local_contexts.jsonld
