8ddd8fa1aad9fed2c31da7b9a6a5b3218fa19305
Jonathan Crabtree <jonathancrabtree@gmail.com>
2018-08-26 15:23:33 -0400

    Summarize differences between the 3 sets of metadata files.

--- bin/gtex_v7_to_dats.py
+++ bin/gtex_v7_to_dats.py
@@ -29,6 +29,66 @@ V7_SAMPLE_ATTRIBUTES_FILE = 'GTEx_v7_Annotations_SampleAttributesDS.txt'
 #RNASEQ_SMAFRZE = 'RNASEQ'
 
 # ------------------------------------------------------
+# Check sample ids between files
+# ------------------------------------------------------
+
+# check for sample and subject ids that appear in the manifest files but not the id dumps
+def cross_check_ids(subjects, samples, manifest, filename, manifest_descr, source_descr):
+    n_samp_found = 0
+    n_samp_not_found = 0
+    sample_d = {}
+    n_subj_found = 0
+    n_subj_not_found = 0
+    subject_d = {}
+
+    n_id_dump_subjects = len(subjects.keys())
+    n_id_dump_samples = len(samples.keys())
+
+    # count distinct subject and sample ids from the specified manifest file
+    for k in manifest:
+        entry = manifest[k]
+
+        # check manifest sample_id
+        sample_id = entry['sample_id']['raw_value']
+        # sample ids should be unique:
+        if sample_id in sample_d:
+            logging.error("found duplicate sample id '" + sample_id + "' in " + filename)
+            continue
+        sample_d[sample_id] = True
+        if sample_id in samples:
+            n_samp_found += 1
+        else:
+            n_samp_not_found += 1
+#            logging.warn("found sample id '" + sample_id + "' in manifest file but not id_dump file")
+
+        # check subject_id
+        m = re.match(r'^((GTEX|K)-[A-Z0-9+]+).*$', sample_id)
+        if m is None:
+            fatal_parse_error("couldn't parse GTEx subject id from sample_id '" + sample_id + "'")
+        subject_id = m.group(1)
+        if subject_id in subject_d:
+            continue
+        else:
+            subject_d[subject_id] = True
+        if subject_id in subjects:
+            n_subj_found += 1
+        else:
+            n_subj_not_found += 1
+#            logging.warn("found subject id '" + subject_id + "' in manifest file but not id_dump file")
+
+    logging.info("comparing GitHub GTEx " + manifest_descr + " manifest files with " + source_descr)
+    samp_compare_str = '{:>10s}  sample_ids in {:>20s}: {:-6} / {:-6}'.format(manifest_descr, source_descr, n_samp_found, n_id_dump_samples) 
+    samp_compare_str += '           '
+    samp_compare_str += '{:>10s}  sample_ids  NOT in {:>20s}: {:-6} / {:-6}'.format(manifest_descr, source_descr, n_samp_not_found, n_id_dump_samples)
+    logging.info(samp_compare_str)
+
+    subj_compare_str = '{:>10s} subject_ids in {:>20s}: {:-6} / {:-6}'.format(manifest_descr, source_descr, n_subj_found,n_id_dump_subjects)
+    subj_compare_str += '           '
+    subj_compare_str += '{:>10s} subject_ids  NOT in {:>20s}: {:-6} / {:-6}'.format(manifest_descr, source_descr, n_subj_not_found, n_id_dump_subjects)
+    logging.info(subj_compare_str)
+
+
+# ------------------------------------------------------
 # main()
 # ------------------------------------------------------
 
@@ -50,25 +110,35 @@ def main():
 #    logging.basicConfig(level=logging.DEBUG)
 
     # read portal metadata for subjects and samples
-    subjects = portal_files.read_subject_phenotypes_file(args.subject_phenotypes_path)
-    samples = portal_files.read_sample_attributes_file(args.sample_attributes_path)
-    portal_files.link_samples_to_subjects(samples, subjects)
+    p_subjects = portal_files.read_subject_phenotypes_file(args.subject_phenotypes_path)
+    p_samples = portal_files.read_sample_attributes_file(args.sample_attributes_path)
+    portal_files.link_samples_to_subjects(p_samples, p_subjects)
 
     # read id dump and manifest files from GitHub data-stewards repo
 
     # id dumps
     subject_id_file = args.data_stewards_repo_path + "/gtex/v7/id_dumps/gtex_v7_subject_ids.txt"
-    subject_ids = github_files.read_subject_id_file(subject_id_file)
+    gh_subjects = github_files.read_subject_id_file(subject_id_file)
     sample_id_file = args.data_stewards_repo_path + "/gtex/v7/id_dumps/gtex_v7_sample_ids.txt"
-    sample_ids = github_files.read_sample_id_file(sample_id_file)
+    gh_samples = github_files.read_sample_id_file(sample_id_file)
     tissue_id_file = args.data_stewards_repo_path + "/gtex/v7/id_dumps/gtex_v7_tissue_ids.txt"
-    tissue_ids = github_files.read_tissue_id_file(tissue_id_file)
+    gh_tissues = github_files.read_tissue_id_file(tissue_id_file)
 
     # manifest files
     protected_rnaseq_manifest = args.data_stewards_repo_path + "/gtex/v7/manifests/protected_data/rnaseq_cram_files_v7_dbGaP_011516.txt"
     protected_rnaseq_files = github_files.read_protected_rnaseq_manifest(protected_rnaseq_manifest)
     protected_wgs_manifest = args.data_stewards_repo_path + "/gtex/v7/manifests/protected_data/wgs_cram_files_v7_hg38_dbGaP_011516.txt"
     protected_wgs_files = github_files.read_protected_wgs_manifest(protected_wgs_manifest)
+
+    # compare GitHub manifest files with GitHub id dumps
+    cross_check_ids(gh_subjects, gh_samples, protected_rnaseq_files, protected_rnaseq_manifest, "RNA-Seq", "GitHub id dumps")
+    cross_check_ids(gh_subjects, gh_samples, protected_wgs_files, protected_wgs_manifest, "WGS","GitHub id dumps")
+
+    # compare GitHub manifest files with GTEx Portal metdata files
+    cross_check_ids(p_subjects, p_samples, protected_rnaseq_files, protected_rnaseq_manifest, "RNA-Seq", "GTEx Portal metadata")
+    cross_check_ids(p_subjects, p_samples, protected_wgs_files, protected_wgs_manifest, "WGS","GTEx Portal metadata")
+
+    # TODO - create DATS subjects and samples based on the manifest files
 
     # create top-level dataset
     gtex_dataset = ccmm.gtex.wgs_datasets.get_dataset_json()
@@ -86,16 +156,12 @@ def main():
             sys.exit(1)
         studies_by_id[m.group(1)] = tds
 
-    # if not processing protected metadata then generate representative DATS JSON using only the public metadata
+    # read public dbGaP metadata
     pub_xp = args.dbgap_public_xml_path
     restricted_mp = args.dbgap_protected_metadata_path
     # read public metadata
     study_pub_md = ccmm.gtex.public_metadata.read_study_metadata(pub_xp)
-#        logging.debug("study_pub_md = " + str(study_pub_md))
     
-    # Study Variables
-    STUDY_VARS = OrderedDict([("value", "Property or Attribute"), ("valueIRI", "http://purl.obolibrary.org/obo/NCIT_C20189")])
-
     # record study variables as dimensions of the study/Dataset
     def add_study_vars(study, study_md):
         # Subject Phenotype study variables
