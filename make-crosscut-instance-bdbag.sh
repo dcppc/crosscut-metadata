2147567b3bde7568268f6adb9652cd6980288621
Jonathan Crabtree <jonathancrabtree@gmail.com>
2018-08-26 09:13:01 -0400

    Increment to v0.5, merge two GTEx commands.

--- make-crosscut-instance-bdbag.sh
+++ make-crosscut-instance-bdbag.sh
@@ -15,28 +15,13 @@ setenv PYTHONPATH ./
 # be expanded to include access-restricted dbGaP metadata (see the relevant script 
 # invocation below) but this expanded instance may not be publicly distributed.
 
-setenv VERSION 0.4
+setenv VERSION 0.5
 setenv EXTERNAL_ID "KC7-crosscut-metadata-v${VERSION}"
 setenv EXTERNAL_DESCR "v${VERSION} release of the KC7 crosscut metadata model for GTEx v7 and TOPMed public metadata using DATS v2.2+"
 
 # set up internal bag structure
 mkdir -p $EXTERNAL_ID/docs
 mkdir -p $EXTERNAL_ID/datasets
-
-## -----------------------------------------------
-## Public GTEx v7 RNA-Seq metadata
-## -----------------------------------------------
-
-# This script requires the following two GTEx metadata files 
-# (from https://www.gtexportal.org/home/datasets to be in the current working directory:
-#
-# GTEx_v7_Annotations_SubjectPhenotypesDS.txt
-# GTEx_v7_Annotations_SampleAttributesDS.txt
-
-./bin/gtex_v7_to_dats.py --output_file=$EXTERNAL_ID/datasets/GTEx_v7_rnaseq_public.jsonld 
-
-# limit to 20 samples to create smaller file for test purposes
-./bin/gtex_v7_to_dats.py --output_file=$EXTERNAL_ID/datasets/GTEx_v7_rnaseq_20_SAMPLES_public.jsonld --max_output_samples=20
 
 ## -----------------------------------------------
 ## Public GTEx v7 dbGaP metadata
@@ -49,7 +34,11 @@ mkdir -p $EXTERNAL_ID/datasets
 #  2. pull ftp://ftp.ncbi.nlm.nih.gov/dbgap/studies/phs000424/phs000424.v7.p2/pheno_variable_summaries/ into dbgap-data/phs000424.v7.p2
 #  3. run the command below
 
-./bin/dbgap_gtex_to_dats.py --dbgap_public_xml_path=./dbgap-data/phs000424.v7.p2 --output_file=$EXTERNAL_ID/datasets/GTEx_v7_dbGaP_public.jsonld
+./bin/gtex_v7_to_dats.py --dbgap_public_xml_path=./dbgap-data/phs000424.v7.p2 \
+  --output_file=$EXTERNAL_ID/datasets/GTEx_v7_public.jsonld
+exit
+
+# TODO - limit to 20 samples to create smaller file for test purposes
 
 ## -----------------------------------------------
 ## RESTRICTED ACCESS GTEx v7 dbGaP metadata
