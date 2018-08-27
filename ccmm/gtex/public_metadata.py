f8bc934efda5d16c6c2f634e7d0765f6de7da1ca
Jonathan Crabtree <jonathancrabtree@gmail.com>
2018-08-27 10:46:14 -0400

    Refactor repeated code for add_study_vars.

--- ccmm/gtex/public_metadata.py
+++ ccmm/gtex/public_metadata.py
@@ -6,3 +6,6 @@ import logging
 # Read all dbGaP XML metadata files in a given directory and read and parse their contents.
 def read_study_metadata(dir):
     return ccmm.dbgap.public_metadata.read_study_metadata(dir)
+
+def add_study_vars(study, study_md):
+    return ccmm.dbgap.public_metadata.add_study_vars(study, study_md)
