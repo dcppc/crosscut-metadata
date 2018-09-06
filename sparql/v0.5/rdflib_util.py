e365202f7e80fbd6ca73614fa76b846860bdb3d0
Jonathan Crabtree <jonathancrabtree@gmail.com>
2018-09-06 10:35:10 -0400

    Add new term definitions needed by RDFLib tabular dumper.

--- sparql/v0.5/rdflib_util.py
+++ sparql/v0.5/rdflib_util.py
@@ -16,17 +16,28 @@ RDF_TYPE_TERM = rdflib.term.URIRef('http://www.w3.org/1999/02/22-rdf-syntax-ns#t
 CENTRAL_ID_TERM = rdflib.term.URIRef('http://purl.obolibrary.org/obo/IAO_0000577')
 NAME_TERM = rdflib.term.URIRef('http://purl.obolibrary.org/obo/IAO_0000590')
 DESCR_TERM = rdflib.term.URIRef('http://purl.obolibrary.org/obo/IAO_0000300')
+DERIVES_FROM_TERM = rdflib.term.URIRef('http://purl.obolibrary.org/obo/RO_0001000')
 TITLE_TERM = rdflib.term.URIRef('http://purl.obolibrary.org/obo/OBI_0001622')
 HAS_PART_TERM = rdflib.term.URIRef('http://purl.obolibrary.org/obo/BFO_0000051')
 PRODUCED_BY_TERM = rdflib.term.URIRef('http://purl.obolibrary.org/obo/RO_0003001')
+HAS_INPUT_TERM = rdflib.term.URIRef('http://purl.obolibrary.org/obo/RO_0002233')
 HAS_MEMBER_TERM = rdflib.term.URIRef('http://purl.obolibrary.org/obo/RO_0002351')
+HAS_QUALITY_TERM = rdflib.term.URIRef('http://purl.obolibrary.org/obo/RO_0000086')
+DATA_ITEM_TERM = rdflib.term.URIRef('http://purl.obolibrary.org/obo/IAO_0000027')
 
+DATS_ANATOMICAL_ENTITY_TERM = rdflib.term.URIRef('http://purl.obolibrary.org/obo/UBERON_0001062')
 DATS_DATASET_TERM = rdflib.term.URIRef('http://purl.obolibrary.org/obo/IAO_0000100')
+DATS_DATA_ACQUISITION_TERM = rdflib.term.URIRef('http://purl.obolibrary.org/obo/OBI_0600013') # information acquisition
 DATS_DIMENSION_TERM = rdflib.term.URIRef('http://purl.obolibrary.org/obo/STATO_23367')
 DATS_MATERIAL_TERM = rdflib.term.URIRef('http://purl.obolibrary.org/obo/BFO_0000040') # "material entity"
 DATS_STUDY_TERM = rdflib.term.URIRef('http://purl.obolibrary.org/obo/OBI_0000066') # "investigation"
 DATS_STUDY_GROUP_TERM = rdflib.term.URIRef('http://purl.obolibrary.org/obo/STATO_0000193') # "study group population"
 
+SDO_ACTION_TERM = rdflib.term.URIRef('https://schema.org/Action')
+SDO_SIZE_TERM = rdflib.term.URIRef('https://schema.org/contentSize')
+SDO_ANATOMICAL_STRUCTURE_TERM = rdflib.term.URIRef('https://schema.org/AnatomicalStructure')
+SDO_DATA_DOWNLOAD_TERM = rdflib.term.URIRef('https://schema.org/DataDownload')
+SDO_DISTRIBUTIONS_TERM = rdflib.term.URIRef('https://schema.org/distribution')
 SDO_IDENT_TERM = rdflib.term.URIRef('https://schema.org/identifier')
 SDO_VALUE_TERM = rdflib.term.URIRef('https://schema.org/value')
 
